95d5b82bdd5287816c08505e538869327a230277
Alex C.R <ninjnova4@hotmail.com>
2024-03-25 14:54:56 +1000

    Added gitignore file and minor changes to sanity_check.py

--- sanity_check.py
+++ sanity_check.py
@@ -92,9 +92,9 @@ def test_solve_weighted_sokoban():
 
 if __name__ == "__main__":
     pass    
-#    print(my_team())  # should print your team
+    # print(my_team())  # should print your team
 
-    #test_taboo_cells() 
-    #test_check_elem_action_seq()
-    #test_solve_weighted_sokoban()
-    print("Hello")
+    # test_taboo_cells()
+    test_check_elem_action_seq()
+    # test_solve_weighted_sokoban()
+    # print("Hello")
